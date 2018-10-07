754951a9912f6d71d65a3dda3c69159029f00030
celestevallejo <Celeste@Celestes-MacBook-Pro.local>
2018-10-07 12:42:48 -0400

    updated legends and plot axis titles

--- Plotting_Scripts/plot_ICA_population.R
+++ Plotting_Scripts/plot_ICA_population.R
@@ -13,7 +13,7 @@ filter_20000_1_pcase = read.table(paste0(dir,'e_and_d_values-1pcase_filter_det_1
 filter_25000_no_pcase = read.table(paste0(dir,'e_and_d_values-0pcase_filter_det_1_N_25000_beta_135_fast.out'), col.names=c('time', 'E&D'))
 filter_25000_1_pcase = read.table(paste0(dir,'e_and_d_values-1pcase_filter_det_1_N_25000_beta_135_fast.out'), col.names=c('time', 'E&D'))
 marker = list(color = brewer.pal(8,"GnBu"))
-png(paste0(dir,'Population_size_on_ED_statistic.png'), width=1200, height=800, res=150)
+png(paste0(dir,'Population_size_on_ED_statistic_ED_pres.png'), width=1200, height=800, res=150)
 plot(filter_3500_no_pcase, type='l',lty=1, col = marker$color[3],xlab = 'Time since last paralytic case (years)',ylab = 'Endemic potential statistic (probability of circulation)',xlim=c(0,5))
   lines(filter_3500_1_pcase,col=marker$color[3],lty=4)
   lines(filter_5000_no_pcase,col=marker$color[4],lty=1)
--- Plotting_Scripts/polio_plots_change_detection.R
+++ Plotting_Scripts/polio_plots_change_detection.R
@@ -2,11 +2,11 @@ dir = '/Users/Celeste/Desktop/polio-small-pop/polio-small-pop/'
 
 library(RColorBrewer)
 
-full_detect_3500 = read.table(paste0(dir,'e_and_d_values-0pcase_filter_det_1_N_3500.out'), col.names=c('time', 'E&D'))
+full_detect_3500 = read.table(paste0(dir,'e_and_d_values-0pcase_filter_det_1_N_3500_beta_135_fast.out'), col.names=c('time', 'E&D'))
 half_detect_3500 = read.table(paste0(dir,'e_and_d_values-0pcase_filter_det_50_N_3500_beta_135.out'), col.names=c('time', 'E&D'))
 quarter_detect_3500 = read.table(paste0(dir,'e_and_d_values-0pcase_filter_det_25_N_3500_beta_135.out'), col.names=c('time', 'E&D'))
 
-full_detect_3500_filter = read.table(paste0(dir,'e_and_d_values-1pcase_filter_det_1_N_3500.out'), col.names=c('time', 'E&D'))
+full_detect_3500_filter = read.table(paste0(dir,'e_and_d_values-1pcase_filter_det_1_N_3500_beta_135_fast.out'), col.names=c('time', 'E&D'))
 half_detect_3500_filter = read.table(paste0(dir,'e_and_d_values-1pcase_filter_det_50_N_3500_beta_135.out'), col.names=c('time', 'E&D'))
 quarter_detect_3500_filter = read.table(paste0(dir,'e_and_d_values-1pcase_filter_det_25_N_3500_beta_135.out'), col.names=c('time', 'E&D'))
 
@@ -26,40 +26,60 @@ full_detect_20000_filter = read.table(paste0(dir,'e_and_d_values-1pcase_filter_d
 half_detect_20000_filter = read.table(paste0(dir,'e_and_d_values-1pcase_filter_det_50_N_20000_beta_135_fast.out'), col.names=c('time', 'E&D'))
 quarter_detect_20000_filter = read.table(paste0(dir,'e_and_d_values-1pcase_filter_det_25_N_20000_beta_135_fast.out'), col.names=c('time', 'E&D'))
 
+full_detect_10000 = read.table(paste0(dir,'e_and_d_values-0pcase_filter_det_1_N_10000_beta_135_fast.out'), col.names=c('time', 'E&D'))
+half_detect_10000 = read.table(paste0(dir,'e_and_d_values-0pcase_filter_det_50_N_10000_beta_135_fast.out'), col.names=c('time', 'E&D'))
+quarter_detect_10000 = read.table(paste0(dir,'e_and_d_values-0pcase_filter_det_25_N_10000_beta_135_fast.out'), col.names=c('time', 'E&D'))
+
+full_detect_10000_filter = read.table(paste0(dir,'e_and_d_values-1pcase_filter_det_1_N_10000_beta_135_fast.out'), col.names=c('time', 'E&D'))
+half_detect_10000_filter = read.table(paste0(dir,'e_and_d_values-1pcase_filter_det_50_N_10000_beta_135_fast.out'), col.names=c('time', 'E&D'))
+quarter_detect_10000_filter = read.table(paste0(dir,'e_and_d_values-1pcase_filter_det_25_N_10000_beta_135_fast.out'), col.names=c('time', 'E&D'))
+
+
 marker = list(color = brewer.pal(8,"GnBu"))
 
-png(paste0(dir,'Effect_of_detection_rate_on_ED_statistic_N_20000.png'), width=1400, height=800, res=150)
-plot(full_detect_20000, type='l',lty=1, xlab='Time since last paralytic case (years)', ylab='E&D stat (probability of circulation)',xlim=c(0,10),col=marker$color[3])
-lines(full_detect_20000_filter,type='l', col=marker$color[3],lty=4)
-lines(half_detect_20000,type='l', col=marker$color[4])
-lines(half_detect_20000_filter,type='l', col=marker$color[4],lty=4)
-lines(quarter_detect_20000,type='l', col=marker$color[5])
-lines(quarter_detect_20000_filter,type='l', col=marker$color[5],lty=4)
-legend('topright', legend = c('ICA, 100% detection','no ICA, 100% detection', 'ICA, 50% detection','no ICA, 50% detection',
-                              'ICA, 25% detection', 'no ICA, 25% detection'),col=c(marker$color[3],marker$color[3],marker$color[4],marker$color[4],marker$color[5],marker$color[5]),lty=c(1,4,1,4,1,4),lwd=2, bty='n' )
+png(paste0(dir,'Effect_of_detection_rate_on_ED_statistic_N_20000_ED_pres.png'), width=1400, height=800, res=150)
+plot(full_detect_20000, type='l',lty=1, xlab='Time since last paralytic case (years)', ylab='Endemic potential statistic (probability of circulation)',xlim=c(0,10),col=marker$color[5])
+lines(full_detect_20000_filter,type='l', col=marker$color[5],lty=4)
+lines(half_detect_20000,type='l', col=marker$color[6])
+lines(half_detect_20000_filter,type='l', col=marker$color[6],lty=4)
+lines(quarter_detect_20000,type='l', col=marker$color[7])
+lines(quarter_detect_20000_filter,type='l', col=marker$color[7],lty=4)
+legend('topright', legend = c('ICA, 100% detection (N=20,000)','no ICA, 100% detection (N=20,000)', 'ICA, 50% detection (N=20,000)','no ICA, 50% detection (N=20,000)',
+                              'ICA, 25% detection (N=20,000)', 'no ICA, 25% detection (N=20,000)'),col=c(marker$color[5],marker$color[5],marker$color[6],marker$color[6],marker$color[7],marker$color[7]),lty=c(1,4,1,4,1,4),lwd=2, bty='n' )
 
 dev.off()
 
-png(paste0(dir,'Effect_of_detection_rate_on_ED_statistic_N_3500.png'), width=1400, height=800, res=150)
-plot(full_detect_3500, type='l',lty=1, xlab='Time since last paralytic case (years)', ylab='E&D stat (probability of circulation)',xlim=c(0,10),col=marker$color[3])
-lines(full_detect_3500_filter, col=marker$color[3],lty=4)
-lines(half_detect_3500, col=marker$color[4])
-lines(half_detect_3500_filter, col=marker$color[4],lty=4)
-lines(quarter_detect_3500, col=marker$color[5])
-lines(quarter_detect_3500_filter, col=marker$color[5],lty=4)
-legend('topright', legend = c('ICA, 100% detection','no ICA, 100% detection', 'ICA, 50% detection','no ICA, 50% detection',
-                              'ICA, 25% detection', 'no ICA, 25% detection'),col=c(marker$color[3],marker$color[3],marker$color[4],marker$color[4],marker$color[5],marker$color[5]),lty=c(1,4,1,4,1,4),lwd=2, bty='n' )
+png(paste0(dir,'Effect_of_detection_rate_on_ED_statistic_N_3500_ED_pres.png'), width=1400, height=800, res=150)
+plot(full_detect_3500, type='l',lty=1, xlab='Time since last paralytic case (years)', ylab='Endemic potential statistic (probability of circulation)',xlim=c(0,10),col=marker$color[5])
+lines(full_detect_3500_filter, col=marker$color[5],lty=4)
+lines(half_detect_3500, col=marker$color[6])
+lines(half_detect_3500_filter, col=marker$color[6],lty=4)
+lines(quarter_detect_3500, col=marker$color[7])
+lines(quarter_detect_3500_filter, col=marker$color[7],lty=4)
+legend('topright', legend = c('ICA, 100% detection (N=3500)','no ICA, 100% detection (N=3500)', 'ICA, 50% detection (N=3500)','no ICA, 50% detection (N=3500)',
+                              'ICA, 25% detection (N=3500)', 'no ICA, 25% detection (N=3500)'),col=c(marker$color[5],marker$color[5],marker$color[6],marker$color[6],marker$color[7],marker$color[7]),lty=c(1,4,1,4,1,4),lwd=2, bty='n' )
+dev.off()
+
+png(paste0(dir,'Effect_of_detection_rate_on_ED_statistic_N_25000_ED_pres.png'), width=1400, height=800, res=150)
+plot(full_detect_25000, type='l',lty=1, xlab='Time since last paralytic case (years)', ylab='Endemic potential statistic (probability of circulation)',xlim=c(0,10),col=marker$color[5])
+lines(full_detect_25000_filter,type='l', col=marker$color[5],lty=4)
+lines(half_detect_25000,type='l', col=marker$color[6])
+lines(half_detect_25000_filter,type='l', col=marker$color[6],lty=4)
+lines(quarter_detect_25000,type='l', col=marker$color[7])
+lines(quarter_detect_25000_filter,type='l', col=marker$color[7],lty=4)
+legend('topright', legend = c('ICA, 100% detection (N=25,000)','no ICA, 100% detection (N=25,000)', 'ICA, 50% detection (N=25,000)','no ICA, 50% detection (N=25,000)',
+                              'ICA, 25% detection (N=25,000)', 'no ICA, 25% detection (N=25,000)'),col=c(marker$color[5],marker$color[5],marker$color[6],marker$color[6],marker$color[7],marker$color[7]),lty=c(1,4,1,4,1,4),lwd=2, bty='n' )
 
 dev.off()
 
-png(paste0(dir,'Effect_of_detection_rate_on_ED_statistic_N_25000.png'), width=1400, height=800, res=150)
-plot(full_detect_25000, type='l',lty=1, xlab='Time since last paralytic case (years)', ylab='E&D stat (probability of circulation)',xlim=c(0,10),col=marker$color[3])
-lines(full_detect_25000_filter,type='l', col=marker$color[3],lty=4)
-lines(half_detect_25000,type='l', col=marker$color[4])
-lines(half_detect_25000_filter,type='l', col=marker$color[4],lty=4)
-lines(quarter_detect_25000,type='l', col=marker$color[5])
-lines(quarter_detect_25000_filter,type='l', col=marker$color[5],lty=4)
-legend('topright', legend = c('ICA, 100% detection','no ICA, 100% detection', 'ICA, 50% detection','no ICA, 50% detection',
-                              'ICA, 25% detection', 'no ICA, 25% detection'),col=c(marker$color[3],marker$color[3],marker$color[4],marker$color[4],marker$color[5],marker$color[5]),lty=c(1,4,1,4,1,4),lwd=2, bty='n' )
+png(paste0(dir,'Effect_of_detection_rate_on_ED_statistic_N_10000_ED_pres.png'), width=1400, height=800, res=150)
+plot(full_detect_10000, type='l',lty=1, xlab='Time since last paralytic case (years)', ylab='Endemic potential statistic (probability of circulation)',xlim=c(0,10),col=marker$color[5])
+lines(full_detect_10000_filter,type='l', col=marker$color[5],lty=4)
+lines(half_detect_10000,type='l', col=marker$color[6])
+lines(half_detect_10000_filter,type='l', col=marker$color[6],lty=4)
+lines(quarter_detect_10000,type='l', col=marker$color[7])
+lines(quarter_detect_10000_filter,type='l', col=marker$color[7],lty=4)
+legend('topright', legend = c('ICA, 100% detection (N=10,000)','no ICA, 100% detection (N=10,000)', 'ICA, 50% detection (N=10,000)','no ICA, 50% detection (N=10,000)',
+                              'ICA, 25% detection (N=10,000)', 'no ICA, 25% detection (N=10,000)'),col=c(marker$color[5],marker$color[5],marker$color[6],marker$color[6],marker$color[7],marker$color[7]),lty=c(1,4,1,4,1,4),lwd=2, bty='n' )
 
 dev.off()
