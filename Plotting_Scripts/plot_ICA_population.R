9dedfbecd88319a221883f24b61ae9532ac85974
celestevallejo <Celeste@Celestes-MacBook-Pro.local>
2018-10-22 11:58:22 -0400

    updated scripts

--- Plotting_Scripts/plot_ICA_population.R
+++ Plotting_Scripts/plot_ICA_population.R
@@ -2,18 +2,18 @@ dir = '/Users/Celeste/Desktop/polio-small-pop/polio-small-pop/'
   
 library(RColorBrewer)
 
-filter_3500_no_pcase = read.table(paste0(dir,'e_and_d_values-0pcase_filter_det_1_N_3500_beta_135_fast.out'), col.names=c('time', 'E&D'))
-filter_3500_1_pcase = read.table(paste0(dir,'e_and_d_values-1pcase_filter_det_1_N_3500_beta_135_fast.out'), col.names=c('time', 'E&D'))
-filter_10000_no_pcase = read.table(paste0(dir,'e_and_d_values-0pcase_filter_det_1_N_10000_beta_135_fast.out'), col.names=c('time', 'E&D'))
-filter_10000_1_pcase = read.table(paste0(dir,'e_and_d_values-1pcase_filter_det_1_N_10000_beta_135_fast.out'), col.names=c('time', 'E&D'))
-filter_5000_no_pcase = read.table(paste0(dir,'e_and_d_values-0pcase_filter_det_1_N_5000_beta_135_fast.out'), col.names=c('time', 'E&D'))
-filter_5000_1_pcase = read.table(paste0(dir,'e_and_d_values-1pcase_filter_det_1_N_5000_beta_135_fast.out'), col.names=c('time', 'E&D'))
-filter_20000_no_pcase = read.table(paste0(dir,'e_and_d_values-0pcase_filter_det_1_N_20000_beta_135_fast.out'), col.names=c('time', 'E&D'))
-filter_20000_1_pcase = read.table(paste0(dir,'e_and_d_values-1pcase_filter_det_1_N_20000_beta_135_fast.out'), col.names=c('time', 'E&D'))
-filter_25000_no_pcase = read.table(paste0(dir,'e_and_d_values-0pcase_filter_det_1_N_25000_beta_135_fast.out'), col.names=c('time', 'E&D'))
-filter_25000_1_pcase = read.table(paste0(dir,'e_and_d_values-1pcase_filter_det_1_N_25000_beta_135_fast.out'), col.names=c('time', 'E&D'))
+filter_3500_no_pcase = read.table(paste0(dir,'e_and_d_values-0pcase_filter_det_1_N_3500_beta_135_fast_response_paper.out'), col.names=c('time', 'E&D'))
+filter_3500_1_pcase = read.table(paste0(dir,'e_and_d_values-1pcase_filter_det_1_N_3500_beta_135_fast_response_paper.out'), col.names=c('time', 'E&D'))
+filter_10000_no_pcase = read.table(paste0(dir,'e_and_d_values-0pcase_filter_det_1_N_10000_beta_135_fast_response_paper.out'), col.names=c('time', 'E&D'))
+filter_10000_1_pcase = read.table(paste0(dir,'e_and_d_values-1pcase_filter_det_1_N_10000_beta_135_fast_response_paper.out'), col.names=c('time', 'E&D'))
+filter_5000_no_pcase = read.table(paste0(dir,'e_and_d_values-0pcase_filter_det_1_N_5000_beta_135_fast_response_paper.out'), col.names=c('time', 'E&D'))
+filter_5000_1_pcase = read.table(paste0(dir,'e_and_d_values-1pcase_filter_det_1_N_5000_beta_135_fast_response_paper.out'), col.names=c('time', 'E&D'))
+filter_20000_no_pcase = read.table(paste0(dir,'e_and_d_values-0pcase_filter_det_1_N_20000_beta_135_fast_response_paper.out'), col.names=c('time', 'E&D'))
+filter_20000_1_pcase = read.table(paste0(dir,'e_and_d_values-1pcase_filter_det_1_N_20000_beta_135_fast_response_paper.out'), col.names=c('time', 'E&D'))
+filter_25000_no_pcase = read.table(paste0(dir,'e_and_d_values-0pcase_filter_det_1_N_25000_beta_135_fast_response_paper.out'), col.names=c('time', 'E&D'))
+filter_25000_1_pcase = read.table(paste0(dir,'e_and_d_values-1pcase_filter_det_1_N_25000_beta_135_fast_response_paper.out'), col.names=c('time', 'E&D'))
 marker = list(color = brewer.pal(8,"GnBu"))
-png(paste0(dir,'Population_size_on_ED_statistic_ED_pres.png'), width=1200, height=800, res=150)
+png(paste0(dir,'Population_size_on_ED_statistic_response_paper.png'), width=1200, height=800, res=150)
 plot(filter_3500_no_pcase, type='l',lty=1, col = marker$color[3],xlab = 'Time since last paralytic case (years)',ylab = 'Endemic potential statistic (probability of circulation)',xlim=c(0,5))
   lines(filter_3500_1_pcase,col=marker$color[3],lty=4)
   lines(filter_5000_no_pcase,col=marker$color[4],lty=1)
@@ -27,4 +27,20 @@ plot(filter_3500_no_pcase, type='l',lty=1, col = marker$color[3],xlab = 'Time si
 legend('topright', legend = c('N=3500, ICA','N=3500, no ICA', 'N=5000, ICA','N=5000, no ICA',
 'N=10000, ICA','N=10000, no ICA',"N=20000, ICA","N=20000, no ICA",'N=25000, ICA','N=25000, no ICA'),col=c(marker$color[3],marker$color[3],marker$color[4],marker$color[4],marker$color[5],marker$color[5],marker$color[6],marker$color[6],marker$color[7],marker$color[7]),lty=c(1,4,1,4,1,4,1,4,1,4),lwd=2, bty='n' )
 dev.off()
+
+filter_22000_no_pcase = read.table(paste0(dir,'e_and_d_values-0pcase_filter_det_1_N_22000_beta_135_fast_response_paper.out'), col.names=c('time', 'E&D'))
+filter_22000_1_pcase = read.table(paste0(dir,'e_and_d_values-1pcase_filter_det_1_N_22000_beta_135_fast_response_paper.out'), col.names=c('time', 'E&D'))
+plot(filter_22000_no_pcase,type='l')
+lines(filter_22000_1_pcase,col='red')
+
+filter_19000_no_pcase = read.table(paste0(dir,'e_and_d_values-0pcase_filter_det_1_N_19000_beta_135_fast_response_paper.out'), col.names=c('time', 'E&D'))
+filter_19000_1_pcase = read.table(paste0(dir,'e_and_d_values-1pcase_filter_det_1_N_19000_beta_135_fast_response_paper.out'), col.names=c('time', 'E&D'))
+plot(filter_19000_no_pcase,type='l')
+lines(filter_19000_1_pcase,col='red')
+
+filter_21000_no_pcase = read.table(paste0(dir,'e_and_d_values-0pcase_filter_det_1_N_21000_beta_135_fast_response_paper.out'), col.names=c('time', 'E&D'))
+filter_21000_1_pcase = read.table(paste0(dir,'e_and_d_values-1pcase_filter_det_1_N_21000_beta_135_fast_response_paper.out'), col.names=c('time', 'E&D'))
+plot(filter_21000_no_pcase,type='l')
+lines(filter_21000_1_pcase,col='red')
+
 
--- Plotting_Scripts/polio_plots_change_detection.R
+++ Plotting_Scripts/polio_plots_change_detection.R
@@ -2,42 +2,42 @@ dir = '/Users/Celeste/Desktop/polio-small-pop/polio-small-pop/'
 
 library(RColorBrewer)
 
-full_detect_3500 = read.table(paste0(dir,'e_and_d_values-0pcase_filter_det_1_N_3500_beta_135_fast.out'), col.names=c('time', 'E&D'))
-half_detect_3500 = read.table(paste0(dir,'e_and_d_values-0pcase_filter_det_50_N_3500_beta_135.out'), col.names=c('time', 'E&D'))
-quarter_detect_3500 = read.table(paste0(dir,'e_and_d_values-0pcase_filter_det_25_N_3500_beta_135.out'), col.names=c('time', 'E&D'))
+full_detect_3500 = read.table(paste0(dir,'e_and_d_values-0pcase_filter_det_1_N_3500_beta_135_fast_response_paper.out'), col.names=c('time', 'E&D'))
+half_detect_3500 = read.table(paste0(dir,'e_and_d_values-0pcase_filter_det_0.5_N_3500_beta_135_fast_response_paper.out'), col.names=c('time', 'E&D'))
+quarter_detect_3500 = read.table(paste0(dir,'e_and_d_values-0pcase_filter_det_0.25_N_3500_beta_135_fast_response_paper.out'), col.names=c('time', 'E&D'))
 
-full_detect_3500_filter = read.table(paste0(dir,'e_and_d_values-1pcase_filter_det_1_N_3500_beta_135_fast.out'), col.names=c('time', 'E&D'))
-half_detect_3500_filter = read.table(paste0(dir,'e_and_d_values-1pcase_filter_det_50_N_3500_beta_135.out'), col.names=c('time', 'E&D'))
-quarter_detect_3500_filter = read.table(paste0(dir,'e_and_d_values-1pcase_filter_det_25_N_3500_beta_135.out'), col.names=c('time', 'E&D'))
+full_detect_3500_filter = read.table(paste0(dir,'e_and_d_values-1pcase_filter_det_1_N_3500_beta_135_fast_response_paper.out'), col.names=c('time', 'E&D'))
+half_detect_3500_filter = read.table(paste0(dir,'e_and_d_values-1pcase_filter_det_0.5_N_3500_beta_135_fast_response_paper.out'), col.names=c('time', 'E&D'))
+quarter_detect_3500_filter = read.table(paste0(dir,'e_and_d_values-1pcase_filter_det_0.25_N_3500_beta_135_fast_response_paper.out'), col.names=c('time', 'E&D'))
 
-full_detect_25000 = read.table(paste0(dir,'e_and_d_values-0pcase_filter_det_1_N_25000_beta_135_fast.out'), col.names=c('time', 'E&D'))
-half_detect_25000 = read.table(paste0(dir,'e_and_d_values-0pcase_filter_det_50_N_25000_beta_135_fast.out'), col.names=c('time', 'E&D'))
-quarter_detect_25000 = read.table(paste0(dir,'e_and_d_values-0pcase_filter_det_25_N_25000_beta_135_fast.out'), col.names=c('time', 'E&D'))
+# full_detect_25000 = read.table(paste0(dir,'e_and_d_values-0pcase_filter_det_1_N_25000_beta_135_fast.out'), col.names=c('time', 'E&D'))
+# half_detect_25000 = read.table(paste0(dir,'e_and_d_values-0pcase_filter_det_50_N_25000_beta_135_fast.out'), col.names=c('time', 'E&D'))
+# quarter_detect_25000 = read.table(paste0(dir,'e_and_d_values-0pcase_filter_det_25_N_25000_beta_135_fast.out'), col.names=c('time', 'E&D'))
+# 
+# full_detect_25000_filter = read.table(paste0(dir,'e_and_d_values-1pcase_filter_det_1_N_25000_beta_135_fast.out'), col.names=c('time', 'E&D'))
+# half_detect_25000_filter = read.table(paste0(dir,'e_and_d_values-1pcase_filter_det_50_N_25000_beta_135_fast.out'), col.names=c('time', 'E&D'))
+# quarter_detect_25000_filter = read.table(paste0(dir,'e_and_d_values-1pcase_filter_det_25_N_25000_beta_135_fast.out'), col.names=c('time', 'E&D'))
 
-full_detect_25000_filter = read.table(paste0(dir,'e_and_d_values-1pcase_filter_det_1_N_25000_beta_135_fast.out'), col.names=c('time', 'E&D'))
-half_detect_25000_filter = read.table(paste0(dir,'e_and_d_values-1pcase_filter_det_50_N_25000_beta_135_fast.out'), col.names=c('time', 'E&D'))
-quarter_detect_25000_filter = read.table(paste0(dir,'e_and_d_values-1pcase_filter_det_25_N_25000_beta_135_fast.out'), col.names=c('time', 'E&D'))
+full_detect_20000 = read.table(paste0(dir,'e_and_d_values-0pcase_filter_det_1_N_20000_beta_135_fast_response_paper.out'), col.names=c('time', 'E&D'))
+half_detect_20000 = read.table(paste0(dir,'e_and_d_values-0pcase_filter_det_0.5_N_20000_beta_135_fast_response_paper.out'), col.names=c('time', 'E&D'))
+quarter_detect_20000 = read.table(paste0(dir,'e_and_d_values-0pcase_filter_det_0.25_N_20000_beta_135_fast_response_paper.out'), col.names=c('time', 'E&D'))
 
-full_detect_20000 = read.table(paste0(dir,'e_and_d_values-0pcase_filter_det_1_N_20000_beta_135_fast.out'), col.names=c('time', 'E&D'))
-half_detect_20000 = read.table(paste0(dir,'e_and_d_values-0pcase_filter_det_50_N_20000_beta_135_fast.out'), col.names=c('time', 'E&D'))
-quarter_detect_20000 = read.table(paste0(dir,'e_and_d_values-0pcase_filter_det_25_N_20000_beta_135_fast.out'), col.names=c('time', 'E&D'))
+full_detect_20000_filter = read.table(paste0(dir,'e_and_d_values-1pcase_filter_det_1_N_20000_beta_135_fast_response_paper.out'), col.names=c('time', 'E&D'))
+half_detect_20000_filter = read.table(paste0(dir,'e_and_d_values-1pcase_filter_det_0.5_N_20000_beta_135_fast_response_paper.out'), col.names=c('time', 'E&D'))
+quarter_detect_20000_filter = read.table(paste0(dir,'e_and_d_values-1pcase_filter_det_0.25_N_20000_beta_135_fast_response_paper.out'), col.names=c('time', 'E&D'))
 
-full_detect_20000_filter = read.table(paste0(dir,'e_and_d_values-1pcase_filter_det_1_N_20000_beta_135_fast.out'), col.names=c('time', 'E&D'))
-half_detect_20000_filter = read.table(paste0(dir,'e_and_d_values-1pcase_filter_det_50_N_20000_beta_135_fast.out'), col.names=c('time', 'E&D'))
-quarter_detect_20000_filter = read.table(paste0(dir,'e_and_d_values-1pcase_filter_det_25_N_20000_beta_135_fast.out'), col.names=c('time', 'E&D'))
+full_detect_10000 = read.table(paste0(dir,'e_and_d_values-0pcase_filter_det_1_N_10000_beta_135_fast_response_paper.out'), col.names=c('time', 'E&D'))
+half_detect_10000 = read.table(paste0(dir,'e_and_d_values-0pcase_filter_det_0.5_N_10000_beta_135_fast_response_paper.out'), col.names=c('time', 'E&D'))
+quarter_detect_10000 = read.table(paste0(dir,'e_and_d_values-0pcase_filter_det_0.25_N_10000_beta_135_fast_response_paper.out'), col.names=c('time', 'E&D'))
 
-full_detect_10000 = read.table(paste0(dir,'e_and_d_values-0pcase_filter_det_1_N_10000_beta_135_fast.out'), col.names=c('time', 'E&D'))
-half_detect_10000 = read.table(paste0(dir,'e_and_d_values-0pcase_filter_det_50_N_10000_beta_135_fast.out'), col.names=c('time', 'E&D'))
-quarter_detect_10000 = read.table(paste0(dir,'e_and_d_values-0pcase_filter_det_25_N_10000_beta_135_fast.out'), col.names=c('time', 'E&D'))
-
-full_detect_10000_filter = read.table(paste0(dir,'e_and_d_values-1pcase_filter_det_1_N_10000_beta_135_fast.out'), col.names=c('time', 'E&D'))
-half_detect_10000_filter = read.table(paste0(dir,'e_and_d_values-1pcase_filter_det_50_N_10000_beta_135_fast.out'), col.names=c('time', 'E&D'))
-quarter_detect_10000_filter = read.table(paste0(dir,'e_and_d_values-1pcase_filter_det_25_N_10000_beta_135_fast.out'), col.names=c('time', 'E&D'))
+full_detect_10000_filter = read.table(paste0(dir,'e_and_d_values-1pcase_filter_det_1_N_10000_beta_135_fast_response_paper.out'), col.names=c('time', 'E&D'))
+half_detect_10000_filter = read.table(paste0(dir,'e_and_d_values-1pcase_filter_det_0.5_N_10000_beta_135_fast_response_paper.out'), col.names=c('time', 'E&D'))
+quarter_detect_10000_filter = read.table(paste0(dir,'e_and_d_values-1pcase_filter_det_0.25_N_10000_beta_135_fast_response_paper.out'), col.names=c('time', 'E&D'))
 
 
 marker = list(color = brewer.pal(8,"GnBu"))
 
-png(paste0(dir,'Effect_of_detection_rate_on_ED_statistic_N_20000_ED_pres.png'), width=1400, height=800, res=150)
+png(paste0(dir,'Effect_of_detection_rate_on_ED_statistic_N_20000_response_paper.png'), width=1400, height=800, res=150)
 plot(full_detect_20000, type='l',lty=1, xlab='Time since last paralytic case (years)', ylab='Endemic potential statistic (probability of circulation)',xlim=c(0,10),col=marker$color[5])
 lines(full_detect_20000_filter,type='l', col=marker$color[5],lty=4)
 lines(half_detect_20000,type='l', col=marker$color[6])
@@ -49,7 +49,7 @@ legend('topright', legend = c('ICA, 100% detection (N=20,000)','no ICA, 100% det
 
 dev.off()
 
-png(paste0(dir,'Effect_of_detection_rate_on_ED_statistic_N_3500_ED_pres.png'), width=1400, height=800, res=150)
+png(paste0(dir,'Effect_of_detection_rate_on_ED_statistic_N_3500_response_paper.png'), width=1400, height=800, res=150)
 plot(full_detect_3500, type='l',lty=1, xlab='Time since last paralytic case (years)', ylab='Endemic potential statistic (probability of circulation)',xlim=c(0,10),col=marker$color[5])
 lines(full_detect_3500_filter, col=marker$color[5],lty=4)
 lines(half_detect_3500, col=marker$color[6])
@@ -60,19 +60,8 @@ legend('topright', legend = c('ICA, 100% detection (N=3500)','no ICA, 100% detec
                               'ICA, 25% detection (N=3500)', 'no ICA, 25% detection (N=3500)'),col=c(marker$color[5],marker$color[5],marker$color[6],marker$color[6],marker$color[7],marker$color[7]),lty=c(1,4,1,4,1,4),lwd=2, bty='n' )
 dev.off()
 
-png(paste0(dir,'Effect_of_detection_rate_on_ED_statistic_N_25000_ED_pres.png'), width=1400, height=800, res=150)
-plot(full_detect_25000, type='l',lty=1, xlab='Time since last paralytic case (years)', ylab='Endemic potential statistic (probability of circulation)',xlim=c(0,10),col=marker$color[5])
-lines(full_detect_25000_filter,type='l', col=marker$color[5],lty=4)
-lines(half_detect_25000,type='l', col=marker$color[6])
-lines(half_detect_25000_filter,type='l', col=marker$color[6],lty=4)
-lines(quarter_detect_25000,type='l', col=marker$color[7])
-lines(quarter_detect_25000_filter,type='l', col=marker$color[7],lty=4)
-legend('topright', legend = c('ICA, 100% detection (N=25,000)','no ICA, 100% detection (N=25,000)', 'ICA, 50% detection (N=25,000)','no ICA, 50% detection (N=25,000)',
-                              'ICA, 25% detection (N=25,000)', 'no ICA, 25% detection (N=25,000)'),col=c(marker$color[5],marker$color[5],marker$color[6],marker$color[6],marker$color[7],marker$color[7]),lty=c(1,4,1,4,1,4),lwd=2, bty='n' )
 
-dev.off()
-
-png(paste0(dir,'Effect_of_detection_rate_on_ED_statistic_N_10000_ED_pres.png'), width=1400, height=800, res=150)
+png(paste0(dir,'Effect_of_detection_rate_on_ED_statistic_N_10000_response_paper.png'), width=1400, height=800, res=150)
 plot(full_detect_10000, type='l',lty=1, xlab='Time since last paralytic case (years)', ylab='Endemic potential statistic (probability of circulation)',xlim=c(0,10),col=marker$color[5])
 lines(full_detect_10000_filter,type='l', col=marker$color[5],lty=4)
 lines(half_detect_10000,type='l', col=marker$color[6])
