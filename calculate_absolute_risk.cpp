d745adba978a12317a3a457211ee95c71a99bf69
celestevallejo <Celeste@Celestes-MacBook-Pro.local>
2018-11-08 20:12:20 -0500

    generalized parameters

--- calculate_absolute_risk.cpp
+++ calculate_absolute_risk.cpp
@@ -17,13 +17,10 @@ int main(int argc, char** argv) {
     //assert(argc == 3);
     string noPcase_filename = argv[1];
     string onePcase_filename = argv[2];
-    int noPcaseSize = 34738;
-    int onePcaseSize = 25635;
-    int numCols = 2;
     
     vector<vector<double>> noPcase_ED_stat;
     vector<vector<double>> onePcase_ED_stat;
-    string output_filename = "absolute_risk_matrix_N_3500.csv";
+    string output_filename = "absolute_risk_matrix_N_5x6000_mig_0_fast_v_slow.csv";
     
     string line;
     double noPcaseValue;
@@ -34,7 +31,6 @@ int main(int argc, char** argv) {
     while(getline(noPcase_file,line)){
         vector<double> row;
         istringstream iss(line);
-        //cout<<line<<endl;
         while(iss >> noPcaseValue){
             row.push_back(noPcaseValue);
         }
@@ -54,20 +50,29 @@ int main(int argc, char** argv) {
     noPcase_file.close();
     onePcase_file.close();
     
-    /*for(int i = 0; i < noPcase_ED_stat.size();i++){
-        for(int j = 0; j < noPcase_ED_stat[j].size();j++){
-            cout<<noPcase_ED_stat[i][j]<<"";
-            if(j==(noPcase_ED_stat[i].size()-1)){
-                cout<<endl;
-            }
-        }
-    }*/
-    
-    vector<vector<double>> absRiskMat(noPcase_ED_stat.size(),vector<double> (2));
+    vector<vector<double>> absRiskMat(onePcase_ED_stat.size(),vector<double> (2));
+    //vector<vector<double>> absRiskMat(noPcase_ED_stat.size(),vector<double> (2));
     
     ofstream fo(output_filename);
     
-    for(int i = 0; i < onePcase_ED_stat.size(); i++){
+    for(int i = 0; i < noPcase_ED_stat.size(); i++){
+        for(int j = 0; j < onePcase_ED_stat.size(); j++){
+            if(i > 1){
+                if(onePcase_ED_stat[j][0] <= noPcase_ED_stat[i][0] and onePcase_ED_stat[j][0]> noPcase_ED_stat[(i-1)][0]){
+                    absRiskMat[j][0] = onePcase_ED_stat[j][0];
+                    absRiskMat[j][1] = onePcase_ED_stat[j][1] - noPcase_ED_stat[i][1];
+                }
+            }
+            else{
+                if(onePcase_ED_stat[j][0] <= noPcase_ED_stat[i][0]){
+                    absRiskMat[j][0] = onePcase_ED_stat[j][0];
+                    absRiskMat[j][1] = onePcase_ED_stat[j][1] - noPcase_ED_stat[i][1];
+                }
+            }
+        }
+    }
+    
+    /*for(int i = 0; i < onePcase_ED_stat.size(); i++){
         for(int j = 0; j < noPcase_ED_stat.size(); j++){
             if(i > 1){
                 if(noPcase_ED_stat[j][0] <= onePcase_ED_stat[i][0] and noPcase_ED_stat[j][0]> onePcase_ED_stat[(i-1)][0]){
@@ -82,7 +87,7 @@ int main(int argc, char** argv) {
                 }
             }
         }
-    }
+    }*/
     
     for(int row = 0; row < absRiskMat.size(); row++){
         for(int col = 0; col < absRiskMat[row].size(); col++){
